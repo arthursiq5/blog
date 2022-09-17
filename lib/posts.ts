acb1068dbf2c14677278de73d2ce92fe552c0463
arthursiq5 <arthur.siq5@gmail.com>
2022-09-17 00:46:04 -0300

    Listados posts

---
+++ lib/posts.ts
@@ -0,0 +1,25 @@
+import { promises as fs } from "fs";
+import matter from "gray-matter";
+import path from "path";
+
+const getPosts = async () => {
+    const postsDirectory = path.join(process.cwd(), 'posts')
+    const filenames = await fs.readdir(postsDirectory)
+
+    return await Promise.all (
+        filenames.map(async (filename) => {
+            const filePath = path.join(postsDirectory, filename)
+            const fileContents = await fs.readFile(filePath, "utf8") // lê o conteúdo do arquivo na pasta de filepath
+            const document = matter(fileContents) // faz o parse dos dados
+
+            return {
+                slug: filename.replace(/\.md$/, ""),
+                title: document.data.title,
+                date: document.data.date,
+                markdown: document.content,
+            }
+        })
+    )
+}
+
+export default getPosts;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,33 @@
 import type { NextPage } from 'next'
+import { AppProps } from 'next/app'
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
+import getPosts from '../lib/posts'
 
-const Home: NextPage = () => {
+const Blog: NextPage<{ posts: object[] }> = ({ posts }) => {
   return (
-    <h1>Blog</h1>
+    <>
+      <h1>Posts</h1>
+      <ul>
+        {posts.map(({slug, title}) => (
+          <li key={slug}>
+            <Link href={`/${slug}`}>
+              <a>{title}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
   )
 }
 
-export default Home
+export async function getStaticProps() {
+  return {
+    props: {
+      posts: await getPosts(),
+    },
+  };
+}
+
+export default Blog
